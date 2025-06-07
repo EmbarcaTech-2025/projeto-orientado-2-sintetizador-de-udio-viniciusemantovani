9911b4cefaef4dad20c2f7e52834f4a36c0b1e42
Vinicius <vinemprego@gmail.com>
2025-06-07 17:30:27 -0300

    Programa com biblioteca de microfone integrada, contendo funçao de leitura ADC e print do microfone

---
+++ src/app/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h> // Biblioteca padrão
+#include <math.h> // Biblioteca de matemática (função "round" foi utilizada)
+
+#include "pico/stdlib.h" // Biblioteca padrão pico
+#include "hardware/gpio.h" // Biblioteca de GPIOs
+#include "hardware/adc.h" // Biblioteca do ADC
+#include "hardware/pwm.h" // Biblioteca do PWM
+
+#include "include/microphone_dma.h" // Biblioteca microfone
+
+int main() {
+  stdio_init_all();
+  
+  sleep_ms(5000);
+
+  adc_gpio_init(MIC_PIN);
+  adc_init();
+  adc_select_input(MIC_CHANNEL);
+
+  adc_fifo_setup(
+    true, // Habilitar FIFO
+    true, // Habilitar request de dados do DMA
+    1, // Threshold para ativar request DMA é 1 leitura do ADC
+    false, // Não usar bit de erro
+    false // Não fazer downscale das amostras para 8-bits, manter 12-bits.
+  );
+
+  adc_set_clkdiv(ADC_CLOCK_DIV);
+
+   // Tomando posse de canal do DMA.
+  dma_channel = dma_claim_unused_channel(true);
+
+  // Configurações do DMA.
+  dma_cfg = dma_channel_get_default_config(dma_channel);
+
+  channel_config_set_transfer_data_size(&dma_cfg, DMA_SIZE_16); // Tamanho da transferência é 16-bits (usamos uint16_t para armazenar valores do ADC)
+  channel_config_set_read_increment(&dma_cfg, false); // Desabilita incremento do ponteiro de leitura (lemos de um único registrador)
+  channel_config_set_write_increment(&dma_cfg, true); // Habilita incremento do ponteiro de escrita (escrevemos em um array/buffer)
+  
+  channel_config_set_dreq(&dma_cfg, DREQ_ADC); // Usamos a requisição de dados do ADC
+
+  while(true)   sample_mic_print();
+  return 0;
+}
